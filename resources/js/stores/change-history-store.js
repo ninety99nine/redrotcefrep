659da31a48fddb0fd13d4dbf75ac4a319f93b2d0
Julian Tabona <brandontabona@gmail.com>
2025-07-31 20:03:33 +0200

    Without compressor for change history

--- resources/js/stores/change-history-store.js
+++ resources/js/stores/change-history-store.js
@@ -1,4 +1,3 @@
-import LZString from 'lz-string';
 import { diff } from 'deep-diff';
 import { defineStore } from 'pinia';
 import debounce from 'lodash.debounce';
@@ -77,7 +76,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
 
       if (this.history.timeline.length === 0) {
         this.history.timeline.unshift({
-          state: LZString.compress(JSON.stringify(serializedData)),
+          state: JSON.stringify(serializedData),
           timestamp: new Date().toISOString(),
           name: actionName,
         });
@@ -90,12 +89,12 @@ export const useChangeHistoryStore = defineStore('change-history', {
         this.history.currentIndex = 0;
       }
 
-      const lastState = JSON.parse(LZString.decompress(this.history.timeline[0].state));
+      const lastState = JSON.parse(this.history.timeline[0].state);
       const differences = diff(lastState, serializedData);
 
       if (differences && differences.length > 0) {
         this.history.timeline.unshift({
-          state: LZString.compress(JSON.stringify(serializedData)),
+          state: JSON.stringify(serializedData),
           timestamp: new Date().toISOString(),
           name: actionName,
         });
@@ -106,7 +105,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
       if (this.canUndo) {
         this.history.currentIndex += 1;
         const previousState = this.history.timeline[this.history.currentIndex];
-        this.data = this.convertBase64ToFiles(JSON.parse(LZString.decompress(previousState.state)));
+        this.data = this.convertBase64ToFiles(JSON.parse(previousState.state));
       } else {
         console.warn('Cannot undo. Already at the earliest state.');
       }
@@ -119,7 +118,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
       if (this.canRedo) {
         this.history.currentIndex -= 1;
         const nextState = this.history.timeline[this.history.currentIndex];
-        this.data = this.convertBase64ToFiles(JSON.parse(LZString.decompress(nextState.state)));
+        this.data = this.convertBase64ToFiles(JSON.parse(nextState.state));
       } else {
         console.warn('Cannot redo. Already at the latest state.');
       }
@@ -132,7 +131,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
       if (index >= 0 && index < this.history.timeline.length) {
         this.history.currentIndex = index;
         const selectedState = this.history.timeline[index];
-        this.data = this.convertBase64ToFiles(JSON.parse(LZString.decompress(selectedState.state)));
+        this.data = this.convertBase64ToFiles(JSON.parse(selectedState.state));
       } else {
         console.warn('Invalid history index.');
       }
@@ -150,7 +149,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
         this.history.currentIndex = 0;
         const originalState = this.history.timeline[this.history.timeline.length - 1];
         this.history.timeline = [originalState];
-        this.data = this.convertBase64ToFiles(JSON.parse(LZString.decompress(originalState.state)));
+        this.data = this.convertBase64ToFiles(JSON.parse(originalState.state));
       }
 
       if (this.listeners.resetHistoryToOriginal) {
@@ -162,7 +161,7 @@ export const useChangeHistoryStore = defineStore('change-history', {
         this.history.currentIndex = 0;
         const currentState = this.history.timeline[0];
         this.history.timeline = [currentState];
-        this.data = this.convertBase64ToFiles(JSON.parse(LZString.decompress(currentState.state)));
+        this.data = this.convertBase64ToFiles(JSON.parse(currentState.state));
       }
 
       if (this.listeners.resetHistoryToCurrent) {
@@ -246,8 +245,8 @@ export const useChangeHistoryStore = defineStore('change-history', {
       if (state.history.timeline.length === 0 || state.history.currentIndex === null) {
         return false;
       }
-      const originalState = JSON.parse(LZString.decompress(state.history.timeline[state.history.timeline.length - 1].state));
-      const currentState = JSON.parse(LZString.decompress(state.history.timeline[state.history.currentIndex].state));
+      const originalState = JSON.parse(state.history.timeline[state.history.timeline.length - 1].state);
+      const currentState = JSON.parse(state.history.timeline[state.history.currentIndex].state);
       const differences = diff(originalState, currentState);
       return differences && differences.length > 0;
     },
