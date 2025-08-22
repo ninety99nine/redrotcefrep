5c0198f482426ff9abadf0f4d791e361d2817a2a
Julian Tabona <brandontabona@gmail.com>
2025-08-22 12:13:37 +0200

    22 Aug 2025 12:13

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Jobs\SendSms;
 
 Route::prefix('auth')->group(function () {
     Route::get('/google', [AuthController::class, 'redirectToGoogle'])->name('social.auth.google');
@@ -11,6 +12,13 @@ Route::prefix('auth')->group(function () {
     Route::get('/linkedin', [AuthController::class, 'redirectToLinkedIn'])->name('social.auth.linkedin');
     Route::get('/linkedin/callback', [AuthController::class, 'handleLinkedInCallback'])->name('social.auth.linkedin.callback');
 });
+
+Route::get('/test-sms', function () {
+
+    SendSms::dispatch('Hello, this is a test sms', '+26772882239');
+    return 'Test sms sent!';
+
+})->where('any', '.*');
 
 Route::get('/{any}', function () {
     return view('welcome');
